89d2c6f5dfb9af9c0170fb3641dadecc4b5aafb1
iamatnuria <ignasiamat10@gmail.com>
2022-04-08 09:14:18 +0200

    Has Many Through in progress

--- database/migrations/2022_04_08_071046_create_affiliations_table.php
+++ database/migrations/2022_04_08_071046_create_affiliations_table.php
@@ -13,15 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('affiliations', function (Blueprint $table) {
             $table->id();
-            $table->unsignedInteger('affiliation_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->integer('experience_points');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -33,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('affiliations');
     }
 };
